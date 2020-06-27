cd1281018aedc8686657520e9418f3e60d6650ee
Leonidas-source <66934329+Leonidas-source@users.noreply.github.com>
2020-06-28 02:40:34 +0700

    Update installarchp3.sh

--- installarchp3.sh
+++ installarchp3.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-lsblk -f | cat >> installarchp3.sh
-vim installarchp3.sh
 #efibootmgr --disk  --part  --create --label "Arch Linux" --loader /vmlinuz-linux --unicode 'root=UUID=  rw initrd=\initramfs-linux.img'
-sh installarchp3.sh
