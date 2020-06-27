b7f62205a2c1442fc68b8cbe7d93b0280123cc9f
Leonidas-source <66934329+Leonidas-source@users.noreply.github.com>
2020-06-27 23:30:09 +0700

    Delete installarchp3.sh

--- installarchp3.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-nano installarchp3.sh
-#efibootmgr --disk /dev/sdX --part Y --create --label "Arch Linux" --loader /vmlinuz-linux --unicode 'root=PARTUUID=XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX  rw initrd=\initramfs-linux.img'
