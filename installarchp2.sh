cfdc05cdd147a5a34ce3fe626639c07e41f9ac3a
Leonidas-source <66934329+Leonidas-source@users.noreply.github.com>
2020-07-05 17:48:44 +0700

    Add files via upload

---
+++ installarchp2.sh
@@ -0,0 +1,152 @@
+#!/bin/bash
+function user {
+	clear
+	echo "Set name for your user"
+	read name
+	useradd -m $name
+	clear
+	echo "Set password for your user"
+	passwd $name
+}
+function heyyy {
+	clear
+	pacman -S sudo
+	EDITOR=nano visudo
+}
+function wm {
+	clear
+	echo "Should I install DE(yes/no)?"
+	read answr2
+	[ "$answr2" == "yes" ] && wmy
+}
+function wmy {
+	clear
+	echo "Set your DE
+	1) plasma
+	2) mate
+	3) gnome
+	4) xfce
+	5) deepin
+	6) cinnamon"
+	read answr3
+	[ "$answr3" == "1" ] && plasma
+	[ "$answr3" == "2" ] && mate
+	[ "$answr3" == "3" ] && gnome
+	[ "$answr3" == "4" ] && xfce
+	[ "$answr3" == "5" ] && deepin
+	[ "$answr3" == "6" ] && cinnamon
+}
+function cinnamon {
+	clear
+	pacman -S cinnamon gdm
+	systemctl enable gdm
+}
+function deepin {
+	clear
+	pacman -S deepin gdm
+	systemctl enable gdm
+}
+function plasma {
+	clear
+	pacman -S plasma-meta
+	systemctl enable sddm
+}
+function mate {
+	clear
+	pacman -S mate mate-extra lxdm
+	systemctl enable lxdm
+}
+function gnome {
+	clear
+	pacman -S gnome
+	systemctl enable gdm
+}
+function xfce {
+	clear
+	pacman -S xfce4 lxdm
+	systemctl enable lxdm
+}
+function system {
+	clear
+	find 2 && grubB || grubU
+}
+function grubB {
+	clear
+	grub-install --target=i386-pc $deviceforinstall
+	clear
+	echo "Do you have Windows installed on your PC(yes/no)?"
+	read winer
+	[ "$winer" == "yes" ] && pacman -S  os-prober
+	clear
+	grub-mkconfig -o /boot/grub/grub.cfg
+}
+function grubU {
+	clear
+	pacman -S efibootmgr
+	grub-install --target=x86_64-efi --efi-directory=esp --bootloader-id=GRUB
+	clear
+	echo "Do you have Windows installed on your PC(yes/no)?"
+	read win
+	[ "$win" == "yes" ] && pacman -S  os-prober
+	clear
+	grub-mkconfig -o /boot/grub/grub.cfg
+}
+function gruby {
+	clear
+	echo "Should I install grub(yes/no)?"
+	read nothing
+	[ "$nothing" == "yes" ] && grubinstall
+}
+function grubinstall {
+	clear
+	pacman -S grub
+	clear
+	lsblk -f
+	echo "Set your drive(not partition) to install grub"
+	read deviceforinstall
+	system
+}
+function pack {
+	clear
+	echo "Should I install some additional packages(yes/no)?"
+	read pick
+	[ "$pick" == "yes" ] && installpack
+}
+function installpack {
+	clear
+	echo "Enter packages to install"
+	read moar
+	clear
+	pacman -S $moar
+}
+clear
+timedatectl set-ntp true
+clear
+nano /etc/locale.gen
+clear
+locale-gen
+clear
+echo "Set your hostname"
+echo "To exit press CTRL+D twice"
+cat > /etc/hostname
+clear
+echo "Set your root password"
+passwd
+clear
+echo "Should I add user(yes/no)?"
+read answr
+[ "$answr" == "yes" ] && user
+clear
+echo "Should I enable dhcpcd(yes/no)?"
+read dhcp
+[ "$dhcp" == "yes" ] && systemctl enable dhcpcd
+clear
+echo "Should I install sudo(yes/no)?"
+read sdo
+[ "$sdo" == "yes" ] && heyyy
+wm
+clear
+gruby
+clear
+pack
+exit 0
