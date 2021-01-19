e7a1bb1f57255906a93de33ae0ebfcd63ba7064e
Leonidas-source <66934329+Leonidas-source@users.noreply.github.com>
2021-01-20 01:56:08 +0700

    Update systemd-boot.sh

--- systemd-boot.sh
+++ systemd-boot.sh
@@ -9,7 +9,7 @@ function loader {
   echo "editor   no" | cat >> loader.conf
 }
 function entry {
-  cd /boot/entries/
+  cd /boot/entries/ || mkdir /boot/entries/
   touch arch.conf
   cd /
   echo "title   Arch Linux" | cat >> /boot/entries/arch.conf
