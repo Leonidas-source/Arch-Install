d6da647b1b2b8f1601476a817acc9fabd173dd85
Leonidas-source <66934329+Leonidas-source@users.noreply.github.com>
2021-07-03 11:10:54 +0700

    Update systemd-boot.sh

--- systemd-boot.sh
+++ systemd-boot.sh
@@ -43,7 +43,7 @@ function systemdpart3 {
 	ls | grep -w "ZEN" && echo "initrd /initramfs-linux-zen.img" >> /mnt/boot/loader/entries/arch.conf
 	clear
 	lsblk
-	echo -e "${red}${bold}Set your root partition(/dev/sda,/dev/sdb...)${reset}"
+	echo -e "${red}${bold}Set your root partition(/dev/sda1,/dev/sdb2,/dev/sdc3...)${reset}"
 	read arg2
 	ls | grep -w "encrypt" && arg3=$(lsblk -fd $arg2 -o UUID | sed s/"UUID"/""/g | sed '/^$/d;s/[[:blank:]]//g')
 	ls | grep -w "encrypt" || arg3=$(lsblk -f $arg2 -o UUID | sed s/"UUID"/""/g | sed '/^$/d;s/[[:blank:]]//g')
