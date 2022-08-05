117fec69669179dfc0309dffd17e0569b2ef19e6
Leonidas-source <66934329+Leonidas-source@users.noreply.github.com>
2022-08-05 20:22:14 +0700

    Update yay.sh

--- yay.sh
+++ yay.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-pacman -S fakeroot autoconf automake bison flex gcc make patch pkgconf
-find /usr/bin/wget || pacman -S wget
+pacman -S fakeroot autoconf automake bison flex gcc make patch pkgconf wget
 pacman -U yay-11.2.0-1-x86_64.pkg.tar.zst
